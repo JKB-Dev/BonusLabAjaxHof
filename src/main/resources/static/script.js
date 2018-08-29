1c9ac5b5aab5160fa227c9bd10f09d02905cd7b5
John Brinker <jkbrinker@gmail.com>
2018-08-29 08:54:56 -0400

    added tiny list data

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -13,19 +13,33 @@ ourRequest.open("GET", "https://dwolverton.github.io/fe-demo/data/computer-scien
 // makes gotten data sendable:
 ourRequest.send();
 ourRequest.onload = function(){
-		
 	var ourData = JSON.parse(ourRequest.responseText);
 	
-	//sort by year here?
-		
 	renderHTML(ourData);  // ---- ourData is unsorted
-	};
-
+//	sortResults(ourData.year, true);
+//	};
+//
+//	function sortResults(year, asc) {
+//	    sortData = sortData.sort(function(a, b) {
+//	        if (asc) {
+//	            return (a[year] > b[year]) ? 1 : ((a[year] < b[year]) ? -1 : 0);
+//	        } else {
+//	            return (b[year] > a[year]) ? 1 : ((b[year] < a[year]) ? -1 : 0);
+//	        }
+//	    });
+//	    renderHTML(sortData);
+	}
+	
 function renderHTML(data) {	// data is a temp variable representing ourData from above
+	
 	for (var i = 0; i < data.complete.length; i++) {
 		var htmlString = "<p>" + data.complete[i].firstName + " " + data.complete[i].lastName + 
 		": " + data.complete[i].innovation + " in " + data.complete[i].year + "<p>";
-		hallDiv.insertAdjacentHTML("beforeend", htmlString); // put HTML string before the end// "name" gets data from API
+		hallDiv.insertAdjacentHTML("beforeend", htmlString);
 	}
 	
+	for (var i = 0; i < data.tiny.length; i++) {
+		var htmlString = "<p>" + data.tiny[i].name + ": " + data.tiny[i].invented + " in " + data.tiny[i].year + "<p>";
+		hallDiv.insertAdjacentHTML("beforeend", htmlString);
+	}
 }
